a20b36216c99175d5650475f0c32b75d08a67d17
bagussetiawansf <bagussetiawan.sf90@gmail.com>
2026-01-09 16:51:55 +0700

    lms ready upload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        Role::firstOrCreate(['name' => 'admin']);
+        Role::firstOrCreate(['name' => 'pendidik']);
+        Role::firstOrCreate(['name' => 'user']);
         
         $pendidik=User::factory()->create([
             'name' => 'Test User',
